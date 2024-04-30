cd0586b990cbccd77416b2886ab6549db0b44d9d
Karthikeya <karthikeya16082@gmail.com>
2024-04-30 01:12:16 -0400

    updated portfolio

--- data/data.js
+++ data/data.js
@@ -57,7 +57,7 @@ export const introductionData = {
 //=========================================
 export const aboutMeData = {
     title: "ABOUT ME",
-    description: "I'm a final year computer science undergraduate student with a keen interest towards data science. I'm currently doing a certified course on 'FULL STACK DATA SCIENCE' from 'INeuron' and I love to browse through problem statements and implement those during my spare time. Checkout my projects at Github link above.",
+    description: "Hey there! I'm Vijay, currently pursuing my master's in data science. I'm passionate about diving into problem statements and crafting innovative solutions using data-driven approaches. When I'm not buried in coursework, you'll often find me tinkering with projects in my spare time. I love putting theory into practice and seeing real-world results. Curious about my work? Check out my projects on GitHub. Let's chat about all things data science!",
     titleURL: "View Resume",
     urlCV: "assets/Resume.pdf"
 }
@@ -88,6 +88,22 @@ export const skillsData = [
         nameSkill: "OpenCV"
     },
     {
+        img: "assets/tableau.svg",
+        nameSkill: "Tableau"
+    },
+    {
+        img: "assets/r.svg",
+        nameSkill: "R"
+    },
+    {
+        img: "assets/unix.svg",
+        nameSkill: "Unix"
+    },
+    {
+        img: "assets/matlab.svg",
+        nameSkill: "matlab"
+    },
+    {
         img: "assets/html-5.svg",
         nameSkill: "HTML"
     },
@@ -112,16 +128,16 @@ export const skillsData = [
 export const projectsData = [
     {
         title: "Lung Cancer Detection Using CNN",    // card image  > insert the svg image in the assets folder
-        describe: "Lung Cancer is one of the leading life taking cancer worldwide. Early detection and treatment are crucial for patient recovery. Medical professionals use histopathological images of biopsied tissue from potentially infected areas of lungs for diagnosis. Most of the time, the diagnosis regarding the types of lung cancer are error-prone and time-consuming. Convolutional Neural networks can identify and classify lung cancer types with greater accuracy in a shorter period, which is crucial for determining patients' right treatment procedure and their survival rate. Benign tissue, Adenocarcinoma, and squamous cell carcinoma are considered in this research work. The CNN model training and validation accuracy of 99.83 and 97.83 percentage are obtained",
+        describe: "Lung Cancer is one of the leading life taking cancer worldwide. Early detection and treatment are crucial for patient recovery. Medical professionals use histopathological images of biopsied tissue from potentially infected areas of lungs for diagnosis. Benign tissue, Adenocarcinoma, and squamous cell carcinoma are considered in this project. The CNN model training and validation accuracy of 87.92 and 86.83 percentage are obtained",
         url: "https://github.com/vijaykhv/Lung-Cancer-Detection-Using-CNN",  // url of the Github repository
         img: "https://i.ibb.co/tHF7cXp/3.png"//image Project
     },
-    // {
-    //     title: "PROJECT TITLE",
-    //     describe: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam veritatis iusto reiciendisquos, delectus in deserunt harum a. Rerum similique corporis sit odit error incidunt distinctio consectetur neque, cum illum?",
-    //     url: "#",
-    //     img: "assets/project1.jpg"
-    // },
+    {
+        title: "UBER FARE PREDICTION USING MACHINE LEARNING MODEL",
+        describe: "In this project, we aim to predict Uber ride fare prices using regression models. We start by cleaning up the dataset, handling missing values, and removing outliers. Then, we build regression models and evaluate their performance using metrics like R2 and RMSE. Our goal is to develop accurate models to help estimate fare prices effectively.",
+        url: "https://github.com/vijaykhv/Uber-Price-Prediction.git",
+        img: "https://i.ibb.co/KGpPSRY/main.png"
+    },
     // {
     //     title: "PROJECT TITLE",
     //     describe: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam veritatis iusto reiciendisquos, delectus in deserunt harum a. Rerum similique corporis sit odit error incidunt distinctio consectetur neque, cum illum?",
